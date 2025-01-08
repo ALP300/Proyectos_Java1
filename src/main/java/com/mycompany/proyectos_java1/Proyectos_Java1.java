b641cb7d1e40988d68adabf091fbc48dd3ae026c
ALP300 <aitorpalomino1@gmail.com>
2025-01-08 18:17:31 -0500

    Crear Repositorio

---
+++ src/main/java/com/mycompany/proyectos_java1/Proyectos_Java1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectos_java1;
+
+/**
+ *
+ * @author aitor
+ */
+public class Proyectos_Java1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
